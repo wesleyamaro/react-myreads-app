c574c73d43ff2eb55e86a9ae811b098d5c7cd74c
Wesley Amaro <wesley.fcamara@duratex.com.br>
2018-05-28 23:47:52 -0300

    type: chore

---
+++ webpack.config.babel.js
@@ -0,0 +1,50 @@
+import { resolve } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({ template: __dirname + '/public/index.html' });
+export default {
+	entry: './src/index.js',
+	output: {
+		path: resolve(__dirname, '/public'),
+        filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	stats: {
+		colors: true,
+		reasons: true
+	},
+	module: {
+		rules: [{
+			test: /\.(s*)css$/,
+			use: ['style-loader', 'css-loader', 'sass-loader']
+		}, {
+			test: /\.jsx?$/,
+			exclude: /node_modules/,
+			loader: ['babel-loader']
+		}, {
+			test: /\.(ttf|eot|woff|woff2)(\?\S*)?$/,
+			loader: 'file-loader',
+			options: {
+			name: '/assets/fonts/[name].[ext]'
+			}
+        }, {
+			test: /\.(jpe?g|png|svg|gif)$/i,
+			loader: 'file-loader',
+			options: {
+			name: '/assets/images/[name].[ext]'
+			}
+        }]
+	},
+	plugins: [
+		HTMLWebpackPluginConfig
+	],
+	devServer: {
+		contentBase: './public',
+		compress: true,
+		port: 8081,
+		watchContentBase: true,
+		clientLogLevel: 'error',
+		historyApiFallback: true
+	},
+};
