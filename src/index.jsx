f15bd2f8513fd423406f1653fbcb737db2ac1423
Wesley Amaro <contato@wesleyamaro.com>
2018-05-30 14:39:13 -0300

    type: refactor moving folders, changing css to sass and deleting icons

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/';
+
+import './assets/styles/main.sass';
+
+ReactDOM.render(<App />, document.getElementById('root'));
