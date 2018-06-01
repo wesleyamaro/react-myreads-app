90934796d844f271f354c7e88793934edd4cc000
Wesley Amaro <contato@wesleyamaro.com>
2018-06-01 14:05:54 -0300

    type: feat adding and configuring react-router-dom package

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import Header from '../Header/';
-import Home from '../../pages/Home/';
 import Footer from '../Footer/';
+import Home from '../../pages/Home/';
+import Search from '../../pages/Search/';
 
 class App extends Component {
 	render() {
@@ -10,7 +12,13 @@ class App extends Component {
 				<Header />
 
 				<main>
-					<Home />
+					<Route exact path="/" render={() => (
+						<Home />
+					)}/>
+
+					<Route path="/search" render={() => (
+						<Search />
+					)}/>
 				</main>
 
 				<Footer />
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Search from '../Search/';
 
 import './index.sass';
@@ -9,7 +10,7 @@ class Header extends Component {
 			<header>
 				<div className="wrapper">
 					<h1>
-						<a href="#">MyReads App</a>
+						<Link to="/">MyReads App</Link>
 					</h1>
 
 					<Search />
