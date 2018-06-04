0d7a2507dd82218532d122d571e08e6b48cdfeea
Wesley Amaro <contato@wesleyamaro.com>
2018-06-04 15:05:29 -0300

    type: feat adding debounce input lib

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import DebounceInput from 'react-debounce-input';
+import PropTypes from 'prop-types';
 
 class Search extends Component {
 	render() {
 		return(
 			<form>
-				<input type="text" name="search" className="large" placeholder="What are you looking for?" />
+				<DebounceInput
+					minLength={3}
+					debounceTimeout={300}
+					className="large"
+					placeholder="What are you looking for?"
+					onChange={this.props.onChangeSearchInput} />
 			</form>
 		);
 	}
 }
+
+Search.propTypes = {
+	onChangeSearchInput: PropTypes.func.isRequired
+};
 
 export default Search;
