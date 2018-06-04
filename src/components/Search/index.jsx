28ffb4bbf730f41d69d9eb1c82fe661df5f00f95
Wesley Amaro <contato@wesleyamaro.com>
2018-06-04 16:24:58 -0300

    type: styles

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import DebounceInput from 'react-debounce-input';
 import PropTypes from 'prop-types';
 
+import './index.sass';
+
 class Search extends Component {
 	render() {
 		return(
-			<form>
-				<DebounceInput
-					minLength={3}
-					debounceTimeout={300}
-					className="large"
-					placeholder="What are you looking for?"
-					onChange={this.props.onChangeSearchInput} />
-			</form>
+			<DebounceInput
+				minLength={3}
+				debounceTimeout={300}
+				className="search-input large"
+				placeholder="What are you looking for?"
+				onChange={this.props.onChangeSearchInput} />
 		);
 	}
 }
