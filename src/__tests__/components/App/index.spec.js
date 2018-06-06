7438699f92eb47249f95c8e822398dc2b23e398f
Wesley Amaro <contato@wesleyamaro.com>
2018-06-06 16:38:49 -0300

    type: test

---
+++ src/__tests__/components/App/index.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import React from 'react';
+//import App from '../../../components/App/';
+
+describe('Testing App component.', () => {
+	it('Should mount Header component.', () => {
+		expect(true).toBe(true);
+	});
+});
